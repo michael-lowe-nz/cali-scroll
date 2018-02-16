1812b06ae21be4f7ba19f62709f58d5f7d2bf2d8
michael-lowe-nz <mikkeyfluttie@gmail.com>
2018-02-16 23:54:13 +1300

    Strip back to something a bit simpler

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import $ from 'jquery'
 import scrollama from 'scrollama';
 import 'animate.css/animate.min.css';
+import './index.scss'
 
-require('./index.scss');
+// require('./index.scss');
 // require('../node_modules/animate.css/animate.css');
 
 // instantiate the scrollama
@@ -10,37 +11,12 @@ const scroller = scrollama();
 
 scroller
   .setup({
-    step: '.scroll__text .step', // required
-    container: '.scroll', // required (for sticky)
-    graphic: '.scroll__graphic', // required (for sticky)
-    offset: 0.4, // optional, default = 0.5
+    step: '.step', // required
+    // container: '.scroll', // required (for sticky)
+    offset: 0.5, // optional, default = 0.5
     debug: false // optional, default = false
   })
-  .onStepEnter((obj, index, another) => {
-    console.log('Step enter:', obj, index, another)
-    if(obj.index === 0) {
-      setTimeout(() => {
-        $('.hero-1-txt').removeClass('hidden');
-        $('.hero-1-txt').addClass('slideInLeft');
-      }, 1000)
-    }
+  .onStepEnter(response => {
+    $(response.element)
+      .addClass('slideInLeft shown');
   })
-  .onStepExit((obj, index) => {
-    // if(obj.index === 0) {
-    //   $('.hero-1-txt').removeClass('fade-in');
-    // }
-  })
-  // .onContainerEnter(() => console.log('Eventing cont-enter'))
-  // .onContainerExit(() => console.log('Eventing cont-exit'))
-
-// scroller
-//   .setup({
-//     step: '.step',
-//     offset: 0.8,
-//     debug: false
-//   })
-//   .onStepEnter(() => {
-//     console.log('Entering step');
-//     $('.hero-1-txt').removeClass('fade-in')
-//   })
-  // .onStepExit(() => $('.hero-1-txt').removeClass('fade-in'));
